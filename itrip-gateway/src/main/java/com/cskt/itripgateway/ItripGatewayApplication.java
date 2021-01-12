c0b3bbba1452851e424f9a820bcfb7af8bf8bde7
ajiu3748 <a_jiu6868@163.com>
2021-01-12 18:50:04 +0800

    添加Gateway网关

---
+++ itrip-gateway/src/main/java/com/cskt/itripgateway/ItripGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cskt.itripgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItripGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItripGatewayApplication.class, args);
+    }
+
+}
---
+++ itrip-gateway/src/test/java/com/cskt/itripgateway/ItripGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cskt.itripgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItripGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
